efa0144180ffd72e579cc3c5fff92ce0ca3b16c7
Eyüp Fidan <info@eyupfidan.com>
2023-08-16 10:36:10 +0300

    Update package version to 1.0.5 and add BacklinkStats endpoint

--- src/endpoints/BacklinkStats.ts
+++ src/endpoints/BacklinkStats.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from "axios";
 
-class DomainRating {
+class BacklinkStats {
   private apiClient: AxiosInstance;
 
   constructor(private token: string) {
@@ -17,17 +17,19 @@ class DomainRating {
     domain: string,
     date: string,
     output: "json" | "csv" | "xml" | "php" | "json",
-    protocol: "both" | "http" | "https" | "both"
+    protocol: "both" | "http" | "https" | "both",
+    mode: "exact" | "prefix" | "domain" | "subdomains" 
   ): Promise<any> {
-    return this.apiClient.get("/domain-rating", {
+    return this.apiClient.get("/backlink-stats", {
       params: {
         target: domain,
         date: date,
         output: output,
         protocol: protocol,
+        mode: mode
       },
     });
   }
 }
 
-export { DomainRating };
+export { BacklinkStats };
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { DomainRating } from './endpoints/DomainRating';
+import { BacklinkStats } from './endpoints/BacklinkStats';
 
 export class SiteExplorer {
     domainRating: DomainRating;
+    backlinkStats: BacklinkStats;
 
     constructor(token: string) {
         this.domainRating = new DomainRating(token);
+        this.backlinkStats = new BacklinkStats(token);
     }
 }
